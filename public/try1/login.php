212fae70b7fb21275ab7d254448568b9955345f5
yamada1023 <141080366+yamada1023@users.noreply.github.com>
2025-04-14 01:09:31 +0000

    try1

--- public/try1/login.php
+++ public/try1/login.php
@@ -8,8 +8,8 @@
   <body class="cyberpunk-bg">
     <div class="login-box">
 	<?php
-		if($_GET['username']=="asosys" &&
-		    $_GET['password']=="abcc2291")
+		if($_POST['username']=="asosys" &&
+		    $_POST['password']=="abcc2291")
 		{
 			echo "<h2>ログイン成功だ！ようこそHACKの世界へ</h2>";
 		}else{
